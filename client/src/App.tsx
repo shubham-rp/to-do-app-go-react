45b1608646b1263c7e668ad04ff02b63da07145b
Shubham Phadte <shubhamphadte37@gmail.com>
2024-05-07 23:56:00 +0530

    Initial changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { Box } from '@mantine/core';
+import useSWR from 'swr';
 import './App.css'
+import AddTodo from './components/AddTodo';
+
+export const ENDPOINT = "http://localhost:4002";
+
+const fetcher = (url: string) => fetch(`${ENDPOINT}/${url}`).then((r)=>r.json());
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const {data, mutate} = useSWR('api/todos', fetcher);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Box>
+      <AddTodo/>
+      Hi
+      {JSON.stringify(data)}
+      </Box>
+    </> 
   )
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { MantineProvider } from '@mantine/core'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <MantineProvider>
     <App />
+    </MantineProvider>
   </React.StrictMode>,
 )
